2f1b7becc88a6ca89fb9be110d08f804e23fa447
TarzEH <70447976+TarzEH@users.noreply.github.com>
2025-12-04 01:03:44 +0700

    Implement database header functions for test

--- src/parse.c
+++ src/parse.c
@@ -10,28 +10,67 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
-
+int create_db_header(struct dbheader_t **headerOut) {
+	struct dbheader_t *header = malloc(sizeof(struct dbheader_t));
+	if (header == NULL) {
+		return STATUS_ERROR;
+	}
+	
+	header->magic = HEADER_MAGIC;
+	header->version = 1;
+	header->count = 0;
+	header->filesize = sizeof(struct dbheader_t);
+	
+	*headerOut = header;
+	return STATUS_SUCCESS;
 }
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
-
+int validate_db_header(int fd, struct dbheader_t **headerOut) {
+	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+	if (header == NULL) {
+		return STATUS_ERROR;
+	}
+	
+	if (read(fd, header, sizeof(struct dbheader_t)) != sizeof(struct dbheader_t)) {
+		free(header);
+		return STATUS_ERROR;
+	}
+	
+	header->version = ntohs(header->version);
+	header->count = ntohs(header->count);
+	header->magic = ntohl(header->magic);
+	header->filesize = ntohl(header->filesize);
+	
+	if (header->magic != HEADER_MAGIC) {
+		free(header);
+		return STATUS_ERROR;
+	}
+	
+	*headerOut = header;
+	return STATUS_SUCCESS;
 }
 
 int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
-
+	return STATUS_SUCCESS;
 }
 
 int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
-
-}	
-
-int validate_db_header(int fd, struct dbheader_t **headerOut) {
-
+	dbhdr->magic = htonl(dbhdr->magic);
+	dbhdr->filesize = htonl(dbhdr->filesize);
+	dbhdr->count = htons(dbhdr->count);
+	dbhdr->version = htons(dbhdr->version);
+	
+	lseek(fd, 0, SEEK_SET);
+	write(fd, dbhdr, sizeof(struct dbheader_t));
+	
+	return STATUS_SUCCESS;
 }
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
-	
+void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
+}
+
+int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
+	return STATUS_SUCCESS;
 }
 
 
